22c40f78f6c891bf4ce2eb79dc13a039cf4d98fa
Shakib Ahmed <112560247+expertskb@users.noreply.github.com>
2023-12-17 12:41:45 +0600

    Update ip.php

--- ip.php
+++ ip.php
@@ -3,6 +3,6 @@ include_once "samdb.class.php";
 
 use Shakib\SAMDb;
 
-$samdb = new SAMDb("dKce5IU7jUqag7lwlslyc1xPEjQ8iSX2");
+$samdb = new SAMDb(""); // Enter You API Token
 
 var_dump($samdb->ip("103.25.249.226"));
